df7d5327f80a236e7eb251a4f6f2787a5164e730
Yuri Yatsenko <115614039+YuriiYatsenko@users.noreply.github.com>
2023-03-27 13:24:48 +0300

    1.0

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,36 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.map(({preview, original, description}) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>
+`).join('');
+
+gallery.innerHTML = galleryMarkup;
+
+gallery.addEventListener('click', (event) => {
+  event.preventDefault();
+  
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  
+  const source = event.target.dataset.source;
+  
+  const modal = basicLightbox.create(`
+    <img src="${source}" width="800" height="600">
+  `);
+  
+  modal.show();
+});
